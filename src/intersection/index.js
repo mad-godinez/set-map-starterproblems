e23be1cdab6c1abb91539731ef9e905c5eede2aa
Mad Godinez <madgodinez@gmail.com>
2022-05-03 15:10:03 -0500

    code solutions are complete and pass all tests

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,41 @@
-function intersection(a, b) {}
+/**
+ * initialize a new Map
+initialize a new Set
+for each element e of a do:
+  add e to the Map
+
+for each element e of b do:
+  lookup e in the Map
+  if e is in the Map then
+    add e to the Set
+convert the Set to an array and return
+ */
+function intersection(a=[],b=[]){
+  if(a.length < 1 || b.length < 1) return [];
+
+  let unique = new Set();
+  b.forEach((value)=> {
+    if(a.includes(value))
+      unique.add(value)
+  });
+
+  return [...unique];
+}
 
 module.exports = intersection;
+
+function solution(a, b){
+  const map = new Map();
+  const results = new Set();
+  for (let e of a) {
+    map.set(e, e);
+  }
+
+  for (let e of b) {
+    if (map.has(e)) {
+      results.add(e);
+    }
+  }
+
+  return Array.from(results);
+}
--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -1,7 +1,53 @@
 /**
+ * Input: an array of words named words
+
+instantiate a new Map named results
+
+for each word in the words array do:
+  lowercase word
+  find all unique letters in word
+    create a Set with all the letters of the word to remove duplicates
+    create an array of letters from the Set and sort alphabetically
+    use join to create a string from the array
+  if the sorted string of letters is a key in the results Map then
+    add word to the array associated with that key
+  else
+    make a new entry in the results Map with key = sorted string of letters and the value an array with word as its only element
+return the results Map
+ * 
  * return a Map of the unique letters and arrays of words that are made up of those letters
- * @param {array} words An array  of words
+ * @param {array} words An array of words
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+  let results = new Map();
+
+  for(let word of words) {
+    word = word.toLowerCase();
+    let letters = new Set(word);
+    let key = [...letters].sort().join('');
+
+    if(results.has(key)) {
+     results.get(key).push(word);
+    }  
+    else
+    results.set(key,[word])  
+  }
+  return results;
+}
 
 module.exports = sameLetters;
+
+function solution(words){
+  const results = new Map();
+  words.forEach((word) => {
+    let distinctLetters = Array.from(new Set(word.split("")))
+      .sort()
+      .join("");
+    if (results.has(distinctLetters)) {
+      results.get(distinctLetters).push(word);
+    } else {
+      results.set(distinctLetters, [word]);
+    }
+  });
+  return results;
+}
